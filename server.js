dc6b3fa680b6a172b0fe8c63aab18b5676e4578f
Evan Turner <evturn@gmail.com>
2015-05-18 02:49:30 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+var express       = require('express');
+var favicon       = require('serve-favicon');
+var logger        = require('morgan');
+var bodyParser    = require('body-parser');
+var cookieParser  = require('cookie-parser');
+var mongoose      = require('mongoose');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var app = express();
+
+var root = __dirname + '/public';
+
+// Handlebars
+var hbs = require('./config/handlebars');
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.engine('hbs', hbs.engine);
+
+// Express Configuration
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'DJ Django D\'jenkins',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(express.static(root));
+
+// Authentication Initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Authentication Configuration
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+    console.log("Server is running on port: Andre Tré-th000usand");
+});
