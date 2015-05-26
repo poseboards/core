80ad18bb5e25d40937f09176cdd9a17eb84a4f79
Evan Turner <evturn@gmail.com>
2015-05-25 22:51:09 -0400

    method-override added for put and delete requests

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express       = require('express');
-var favicon       = require('serve-favicon');
-var logger        = require('morgan');
-var bodyParser    = require('body-parser');
-var cookieParser  = require('cookie-parser');
-var mongoose      = require('mongoose');
-var passport      = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var db = require('./config/db')(mongoose);
+var express        = require('express');
+var methodOverride = require('method-override');
+var logger         = require('morgan');
+var bodyParser     = require('body-parser');
+var cookieParser   = require('cookie-parser');
+var mongoose       = require('mongoose');
+var db             = require('./config/db')(mongoose);
+var passport       = require('passport');
+var hbs            = require('./config/handlebars');
+var LocalStrategy  = require('passport-local').Strategy;
 
 var app = express();
 
 var root = __dirname + '/public';
 
 // Handlebars
-var hbs = require('./config/handlebars');
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.engine('hbs', hbs.engine);
@@ -22,6 +22,13 @@ app.engine('hbs', hbs.engine);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 app.use(cookieParser());
 app.use(require('express-session')({
   secret: 'DJ Django D\'jenkins',
